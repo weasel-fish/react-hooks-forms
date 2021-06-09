74fb6f048c3ac3f3d71588b86ffd2d90d8896584
github username <github email address>
2021-06-09 08:26:23 -0400

    commit tree 3d25e0c5cec4299019af94029d61046cb1db2528

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,18 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(e){
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value)
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input onChange={handleFirstNameChange} type="text" value={firstName} />
+      <input onChange={handleLastNameChange} type="text" value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
